93609ba409cdf9372d893d3b6bd619548638d946
板谷大哉 <daiya199778@gmail.com>
2023-02-08 13:44:04 +0900

    ユーザー登録画面_実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#top'
+
+  #ログイン機能のルーティング
+  get 'login', to: 'user_session#new'
+  post 'login', to: 'user_session#create'
+  delete 'logout', to: 'user_session#destroy'
+
+  #ユーザーの新規登録機能
+  resources :users, only: %i[new create]
 end
