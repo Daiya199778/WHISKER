624bc8a8a1c848798514720aabee1a0d0f40512b
板谷大哉 <daiya199778@gmail.com>
2023-02-10 18:43:09 +0900

    user_sessions_controllerの編集と翻訳の再設定

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,8 +5,8 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      #投稿ページにアクセスしようとしたユーザにログインを要求する場合、require_loginメソッドでユーザをログインページに誘導し、ログインが成功したら、最初に訪れようとしていた掲示板ページにリダイレクトさせる
-      redirect_back_or_to root_path, success: t('.success')
+      #ログイン認証に成功したら投稿一覧画面へ遷移するようにする
+      redirect_back_or_to posts_path, success: t('.success')
     else
       flash.now[:danger] = t('.fail')
       render :new
