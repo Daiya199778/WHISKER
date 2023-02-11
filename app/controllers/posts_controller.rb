361e6751907429f0a6e221a71fd01191b917fab5
板谷大哉 <daiya199778@gmail.com>
2023-02-11 12:49:09 +0900

    新規投稿_controllerとroutesファイルの編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,32 @@
 class PostsController < ApplicationController
   def index
+    #投稿のデータを引っ張ってくる際に、関連付けされたモデルのデータも一緒に取得するために「includes(user)」を使う
     @posts = Post.all.includes(:user).order(created_at: :desc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    #current_user.posts.build=ログインしているユーザーの投稿を@postへ代入する
+    @post = current_user.posts.build(post_params)
+    #binding.pry      
+    if @post.save
+      redirect_to posts_path, success: t('posts.create.success')
+    else
+      flash.now['danger'] = t('posts.create.fail')
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :image, :price, :whiskey_brand)
   end
 end
