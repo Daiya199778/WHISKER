e77be81d20af88a1eb28ddfc23b80a4ddeb5b65a
板谷大哉 <daiya199778@gmail.com>
2023-02-16 17:01:06 +0900

    バリデーションの実装とbootstrapのアイコン追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,10 @@ class Post < ApplicationRecord
   
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
+  validates :price, presence: true
   validates :whiskey_brand, presence: true, length: { maximum: 255 }
   validates :countries, presence: true
+  validates :whiskey_types, presence: true
 
   #post(投稿)は「原産国」「原材料」と1対多の関係
   has_many :countries, dependent: :destroy
