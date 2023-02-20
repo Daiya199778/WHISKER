1fca5f679a1a285cdd4694c0058738392fe8f940
板谷大哉 <daiya199778@gmail.com>
2023-02-20 13:55:37 +0900

    postsコントローラーにbookmarksアクションの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -50,6 +50,10 @@ class PostsController < ApplicationController
     redirect_to posts_path, success: t('posts.destroy.success')
   end
 
+  def bookmarks
+    @bookmark_posts = current_user.bookmark_posts.includes(:user).order(created_at: :desc)
+  end
+
   private
 
   def set_post
