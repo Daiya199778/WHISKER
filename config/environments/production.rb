fcacf6922b1857a38748fd0b0334ed03db71b23e
板谷大哉 <daiya199778@gmail.com>
2023-03-30 12:19:48 +0900

    configの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,11 +23,10 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
 
-  #コメントアウト
-  #config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   #コメント追加
-  config.public_file_server.enabled = true
+  #config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
