ca15e64d48e95286a1b69abece65063c85c71a14
板谷大哉 <daiya199778@gmail.com>
2023-04-01 02:14:07 +0900

    s3_編集2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,13 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+unless Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: 'AKIASM4ZUJ5HHUERBP7U',
+      aws_secret_access_key: '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',
+      region: 'ap-northeast-1'
+    }
 
-CarrierWave.configure do |config|
-  config.storage :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'whisker-backet'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_DEFAULT_REGION'],
-    path_style: true
-  }
-
+    config.fog_directory  = 'whisker-backet'
+    config.cache_storage = :fog
+  end
 end
