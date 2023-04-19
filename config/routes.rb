fda1aff784e8332a285dd04ec85444663dcda227
板谷大哉 <daiya199778@gmail.com>
2023-04-19 22:18:16 +0900

    マイグレーションファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -51,4 +51,6 @@ Rails.application.routes.draw do
   # ブックマークのcreateアクションとdestroyアクション
   resources :bookmarks, only: %i[create destroy]
   resources :profiles, only: %i[show edit update]
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_07_043957) do
+ActiveRecord::Schema.define(version: 2023_04_19_131108) do
 
   create_table "authentications", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -82,7 +82,12 @@ ActiveRecord::Schema.define(version: 2023_04_07_043957) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "introduction"
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
   create_table "whiskey_types", force: :cascade do |t|
