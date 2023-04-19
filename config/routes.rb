85d7024f50a0159c388d7348f80ac552ec337cb2
板谷大哉 <daiya199778@gmail.com>
2023-04-19 17:26:25 +0900

    password_reset_new_実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#top'
 
+  #パスワードリセットのルーティング
+  resources :password_resets, only: %i[new create edit update]
+  #get "password_reset/edit", to: "passoword_reset#edit"
+
+
   #ウイスキー豆知識のルーティング
   resource :konwledge, only: %i[show] do
     collection do
